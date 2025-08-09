151a3302ae9290a4d2e3c41bec2a480be1cae859
EduRibeiro00 <eribeiro306@gmail.com>
2025-08-09 21:57:06 +0200

    Actual final change on code blocks for now

--- components/custom/code-block.tsx
+++ components/custom/code-block.tsx
@@ -1,37 +1,32 @@
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { coldarkCold } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
-/**
- * Importing only the needed languages to reduce bundle size
-//  */
-// import javascript from "react-syntax-highlighter/dist/cjs/languages/hljs/javascript";
-// import python from "react-syntax-highlighter/dist/cjs/languages/hljs/python";
-// import scala from "react-syntax-highlighter/dist/cjs/languages/hljs/scala";
-// import typescript from "react-syntax-highlighter/dist/cjs/languages/hljs/typescript";
-
-// SyntaxHighlighter.registerLanguage("scala", scala);
-// SyntaxHighlighter.registerLanguage("javascript", javascript);
-// SyntaxHighlighter.registerLanguage("typescript", typescript);
-// SyntaxHighlighter.registerLanguage("python", python);
-
 interface CodeBlockProps {
   children: string;
   language?: string;
 }
 
 export default function CodeBlock({ children, language }: CodeBlockProps) {
+  const code = String(children).replace(/\n$/, "");
+
   return (
-    <SyntaxHighlighter
-      style={coldarkCold}
-      language={language ?? "plaintext"}
-      customStyle={{
-        background: "transparent",
-        fontSize: "0.9em",
-        margin: 0,
-        padding: 0,
-      }}
-    >
-      {String(children).replace(/\n$/, "")}
-    </SyntaxHighlighter>
+    <div className="group relative overflow-auto text-sm">
+      {/* TODO: add copy button and language title */}
+      {/* <div className="absolute right-0 text-gray-500">
+        <Copy className="w-4 h-4" />
+      </div> */}
+      <SyntaxHighlighter
+        style={coldarkCold}
+        language={language ?? "plaintext"}
+        customStyle={{
+          background: "transparent",
+          font: "mono",
+          margin: 0,
+          padding: 0,
+        }}
+      >
+        {code}
+      </SyntaxHighlighter>
+    </div>
   );
 }
